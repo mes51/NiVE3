15dfb846ca679ec69f175530b6ca589b001b2667
mes51 <mes.goruba@gmail.com>
2023-02-27 21:34:02 +0900

    LayerModel追加

--- NiVE3/Model/CompositionModel.cs
+++ NiVE3/Model/CompositionModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,28 @@ namespace NiVE3.Model
 {
     class CompositionModel : BindableBase
     {
+        private ObservableCollection<LayerModel> layers = new ObservableCollection<LayerModel>();
+        public ObservableCollection<LayerModel> Layers
+        {
+            get { return layers; }
+            set
+            {
+                if (layers != value)
+                {
+                    layers.CollectionChanged -= Layers_CollectionChanged;
+                    value.CollectionChanged += Layers_CollectionChanged;
+                }
+                SetProperty(ref layers, value);
+            }
+        }
+
+        public CompositionModel()
+        {
+            Layers = new ObservableCollection<LayerModel>();
+        }
+
+        private void Layers_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+        }
     }
 }
---
+++ NiVE3/Model/LayerModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace NiVE3.Model
+{
+    class LayerModel : BindableBase
+    {
+        private ObservableCollection<EffectModel> effects = new ObservableCollection<EffectModel>();
+        public ObservableCollection<EffectModel> Effects
+        {
+            get { return effects; }
+            set
+            {
+                if (effects!= value)
+                {
+                    effects.CollectionChanged -= Effects_CollectionChanged;
+                    value.CollectionChanged += Effects_CollectionChanged;
+                }
+                SetProperty(ref effects, value);
+            }
+        }
+
+        public LayerModel()
+        {
+            Effects = new ObservableCollection<EffectModel>();
+        }
+
+        private void Effects_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+        }
+    }
+}
