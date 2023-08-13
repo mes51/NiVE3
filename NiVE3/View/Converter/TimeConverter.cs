9e6e5cbfa2b55e4542856e7ca813bf9de4793cc4
mes51 <mes.goruba@gmail.com>
2023-08-13 15:47:35 +0900

    DurationConverterが現実に即していないためリネーム

--- NiVE3/View/Converter/TimeConverter.cs
+++ NiVE3/View/Converter/TimeConverter.cs
@@ -6,55 +6,95 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using NiVE3.UI.Converter;
 
 namespace NiVE3.View.Converter
 {
-    class TimeConverter : IMultiValueConverter
+    [ValueConversion(typeof(double), typeof(string))]
+    class TimeConverter : Freezable, IValueConverter
     {
-        /// <summary>
-        /// 長さとフレームレートから時間表記の文字列に変換する
-        /// </summary>
-        /// <param name="values">
-        /// 0: duration
-        /// 1: frame rate
-        /// </param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        public static readonly DependencyProperty FrameRateProperty = DependencyProperty.Register(
+            nameof(FrameRate),
+            typeof(double),
+            typeof(TimeConverter),
+            new PropertyMetadata(1.0)
+        );
+
+        public double FrameRate
         {
-            if (values.Any(v => v is not double))
+            get { return (double)GetValue(FrameRateProperty); }
+            set { SetValue(FrameRateProperty, value); }
+        }
+
+        CalcDoubleConverter Converter { get; } = new CalcDoubleConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double time)
             {
                 return DependencyProperty.UnsetValue;
             }
-            else if (values.Length < 2)
-            {
-                throw new ArgumentOutOfRangeException(nameof(values));
-            }
 
-            var time = (double)values[0];
-            var frameRate = (double)values[1];
             var hour = (int)(time / 3600);
             var minute = (int)((time % 3600) / 60);
             var second = (int)(time % 60);
-            var frame = (int)((time % 1) * frameRate);
+            var frame = (int)((time % 1) * FrameRate);
 
             return $"{hour}:{minute:D2}:{second:D2}:{frame:D2}";
         }
 
-        /// <summary>
-        /// 非対応
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetTypes"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return Enumerable.Repeat(DependencyProperty.UnsetValue, targetTypes.Length).ToArray();
+            if (value is not string timeText)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            var part = timeText.Split(":");
+            Array.Reverse(part);
+            var time = 0.0;
+            if (part.Length > 0)
+            {
+                time += ParseValue(part[0]) / FrameRate;
+            }
+            if (part.Length > 1)
+            {
+                time += ParseValue(part[1]);
+            }
+            if (part.Length > 2)
+            {
+                time += ParseValue(part[2]) * 60.0;
+            }
+            if (part.Length > 3)
+            {
+                time += ParseValue(part[2]) * 3600.0;
+            }
+
+            if (double.IsNaN(time))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            else
+            {
+                return time;
+            }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new TimeConverter();
+        }
+
+        double ParseValue(string text)
+        {
+            if (Converter.ConvertBack(text, typeof(double), null, CultureInfo.InvariantCulture) is double v)
+            {
+                return v;
+            }
+            else
+            {
+                return double.NaN;
+            }
         }
     }
 }
---
+++ NiVE3/View/Converter/TimeMultiValueConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NiVE3.View.Converter
+{
+    class TimeMultiValueConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// 長さとフレームレートから時間表記の文字列に変換する
+        /// </summary>
+        /// <param name="values">
+        /// 0: duration
+        /// 1: frame rate
+        /// </param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Any(v => v is not double))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            else if (values.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(values));
+            }
+
+            var time = (double)values[0];
+            var frameRate = (double)values[1];
+            var hour = (int)(time / 3600);
+            var minute = (int)((time % 3600) / 60);
+            var second = (int)(time % 60);
+            var frame = (int)((time % 1) * frameRate);
+
+            return $"{hour}:{minute:D2}:{second:D2}:{frame:D2}";
+        }
+
+        /// <summary>
+        /// 非対応
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetTypes"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return Enumerable.Repeat(DependencyProperty.UnsetValue, targetTypes.Length).ToArray();
+        }
+    }
+}
