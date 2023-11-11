fdad3918e32862a294f13e1e245515ff603dc5a4
mes51 <mes.goruba@gmail.com>
2023-11-11 22:55:26 +0900

    ライトレイヤーを親として反映できるように
    *
    通常のレイヤーにカメラを親に設定したときのPoIとPositionがひっくり返っていたのを修正

--- NiVE3.PresetPlugin/Renderer/DefaultRenderer.cs
+++ NiVE3.PresetPlugin/Renderer/DefaultRenderer.cs
@@ -77,24 +77,7 @@ namespace NiVE3.PresetPlugin.Renderer
             var size = Math.Max(Width, Height);
             var fov = Math.Atan((Width / cameraSetting.Zoom) * 0.5) * 2.0;
 
-            var view = GetCameraMatrix(cameraSetting, Width, Height);
-            foreach (var (type, parentTransform) in cameraSetting.ParentTransforms)
-            {
-                switch (type)
-                {
-                    case ParentType.Camera:
-                        view = GetCameraMatrix(parentTransform, Width, Height) * view;
-                        break;
-                    default:
-                        if (Matrix4x4d.Invert(GetTransform3D(parentTransform, size), out var inverted))
-                        {
-                            view = inverted * view;
-                        }
-                        break;
-                }
-            }
-
-            ViewMatrix = view.Translate(-(size - Width) * 0.5 / size, -(size - Height) * 0.5 / size, 0.0);
+            ViewMatrix = Calc3DViewMatrix(cameraSetting, Width, Height);
             ProjectionMatrix = Matrix4x4d.CreatePerspectiveFieldOfView(fov, 1.0, double.Epsilon, double.PositiveInfinity);
         }
 
@@ -121,22 +104,8 @@ namespace NiVE3.PresetPlugin.Renderer
                     foreach (var i in group)
                     {
                         var opacity = (double)(i.Transform[ILayerObject.TransformPropertyOpacityId] ?? 0.0) * 0.01;
-                        var model = GetTransform3D(i.Transform, renderer.Size);
 
-                        foreach (var (type, parentTransform) in i.ParentTransforms)
-                        {
-                            switch (type)
-                            {
-                                case ParentType.Camera:
-                                    model *= GetInvertedCameraMatrix(parentTransform, Width, Height);
-                                    break;
-                                default:
-                                    model *= GetTransform3D(parentTransform, renderer.Size);
-                                    break;
-                            }
-                        }
-
-                        renderer.ModelMatrix = model;
+                        renderer.ModelMatrix = Calc3DModelMatrix(i.Transform, i.ParentTransforms, Width, Height);
                         renderer.AddRect(i.Image, i.BlendMode);
                     }
 
@@ -149,12 +118,7 @@ namespace NiVE3.PresetPlugin.Renderer
                     foreach (var i in group)
                     {
                         var opacity = (double)(i.Transform[ILayerObject.TransformPropertyOpacityId] ?? 0.0) * 0.01;
-                        var matrix = GetTransform2D(i.Transform);
-
-                        foreach (var(_, parentTransform) in i.ParentTransforms)
-                        {
-                            matrix *= GetTransform2D(parentTransform);
-                        }
+                        var matrix = CalcTransform2D(i.Transform, i.ParentTransforms);
 
                         renderer.Draw(i.Image, (float)opacity, matrix, i.InterpolationQuality, i.BlendMode);
                     }
@@ -186,12 +150,8 @@ namespace NiVE3.PresetPlugin.Renderer
 
         public PreviewBoundingBox CalcBoundingBox2D(int width, int height, PropertyValueGroup transform, ParentTransform[] parentTransforms)
         {
-            var matrix = GetTransform2D(transform);
+            var matrix = CalcTransform2D(transform, parentTransforms);
             var anchorPoint = (Vector3d)(transform[ILayerObject.TransformAnchorPointId] ?? new Vector3d());
-            foreach (var (_, parentTransform) in parentTransforms)
-            {
-                matrix *= GetTransform2D(parentTransform);
-            }
 
             return new PreviewBoundingBox(
                 (Vector2d)matrix.Transform(new Vector2(0.0F, 0.0F)),
@@ -209,38 +169,8 @@ namespace NiVE3.PresetPlugin.Renderer
             var anchorPoint = (Vector3d)(transform[ILayerObject.TransformAnchorPointId] ?? new Vector3d());
 
             var projectionMatrix = Matrix4x4d.CreatePerspectiveFieldOfView(fov, 1.0, double.Epsilon, double.PositiveInfinity);
-            var modelMatrix = GetTransform3D(transform, size);
-
-            var view = GetCameraMatrix(cameraSetting, Width, Height);
-            foreach (var (type, parentTransform) in cameraSetting.ParentTransforms)
-            {
-                switch (type)
-                {
-                    case ParentType.Camera:
-                        view = GetCameraMatrix(parentTransform, Width, Height) * view;
-                        break;
-                    default:
-                        if (Matrix4x4d.Invert(GetTransform3D(parentTransform, size), out var inverted))
-                        {
-                            view = inverted * view;
-                        }
-                        break;
-                }
-            }
-            var viewMatrix = view.Translate(-(size - Width) * 0.5 / size, -(size - Height) * 0.5 / size, 0.0);
-
-            foreach (var (type, parentTransform) in parentTransforms)
-            {
-                switch (type)
-                {
-                    case ParentType.Camera:
-                        modelMatrix *= GetInvertedCameraMatrix(parentTransform, Width, Height);
-                        break;
-                    default:
-                        modelMatrix *= GetTransform3D(parentTransform, size);
-                        break;
-                }
-            }
+            var modelMatrix = Calc3DModelMatrix(transform, parentTransforms, Width, Height);
+            var viewMatrix = Calc3DViewMatrix(cameraSetting, Width, Height);
 
             var mv = modelMatrix * viewMatrix * Matrix4x4d.CreateTranslate((size - Width) * 0.5 / size, (size - Height) * 0.5 / size, 0.0);
 
@@ -295,6 +225,72 @@ namespace NiVE3.PresetPlugin.Renderer
                     bbAnchorPoint
                 );
             }
+        }
+
+        static Matrix3x3 CalcTransform2D(PropertyValueGroup transform, ParentTransform[] parentTransforms)
+        {
+            var matrix = GetTransform2D(transform);
+            foreach (var (_, parentTransform) in parentTransforms)
+            {
+                matrix *= GetTransform2D(parentTransform);
+            }
+
+            return matrix;
+        }
+
+        static Matrix4x4d Calc3DModelMatrix(PropertyValueGroup transform, ParentTransform[] parentTransforms, double renderWidth, double renderHeight)
+        {
+            var size = Math.Max(renderWidth, renderHeight);
+            var matrix = GetTransform3D(transform, size);
+
+            foreach (var (type, parentTransform) in parentTransforms)
+            {
+                switch (type)
+                {
+                    case ParentType.Camera:
+                        matrix *= GetInvertedCameraMatrix(parentTransform, renderWidth, renderHeight);
+                        break;
+                    case ParentType.SpotOrParallelLight:
+                    case ParentType.PointLight:
+                        matrix *= GetInvertedLightMatrix(type == ParentType.SpotOrParallelLight ? LightType.Spot : LightType.Point, parentTransform, renderWidth, renderHeight);
+                        break;
+                    case ParentType.AmbientLight:
+                        break;
+                    default:
+                        matrix *= GetTransform3D(parentTransform, size);
+                        break;
+                }
+            }
+
+            return matrix;
+        }
+
+        static Matrix4x4d Calc3DViewMatrix(CameraSetting cameraSetting, double renderWidth, double renderHeight)
+        {
+            var size = Math.Max(renderWidth, renderHeight);
+            var view = GetCameraMatrix(cameraSetting, renderWidth, renderHeight);
+            foreach (var (type, parentTransform) in cameraSetting.ParentTransforms)
+            {
+                switch (type)
+                {
+                    case ParentType.Camera:
+                        view = GetCameraMatrix(parentTransform, renderWidth, renderHeight) * view;
+                        break;
+                    case ParentType.SpotOrParallelLight:
+                    case ParentType.PointLight:
+                        view = GetLightMatrix(type == ParentType.SpotOrParallelLight ? LightType.Spot : LightType.Point, parentTransform, renderWidth, renderHeight) * view;
+                        break;
+                    case ParentType.AmbientLight:
+                        break;
+                    default:
+                        if (Matrix4x4d.Invert(GetTransform3D(parentTransform, size), out var inverted))
+                        {
+                            view = inverted * view;
+                        }
+                        break;
+                }
+            }
+            return view.Translate(-(size - renderWidth) * 0.5 / size, -(size - renderHeight) * 0.5 / size, 0.0);
         }
 
         static Matrix3x3 GetTransform2D(PropertyValueGroup transform)
@@ -367,8 +363,8 @@ namespace NiVE3.PresetPlugin.Renderer
 
         static Matrix4x4d GetInvertedCameraMatrix(PropertyValueGroup transform, double renderWidth, double renderHeight)
         {
-            var pos = (Vector3d)(transform[ILayerObject.TransformPointOfInterestId] ?? new Vector3d());
-            var poi = (Vector3d)(transform[ILayerObject.TransformPositionId] ?? new Vector3d());
+            var poi = (Vector3d)(transform[ILayerObject.TransformPointOfInterestId] ?? new Vector3d());
+            var pos = (Vector3d)(transform[ILayerObject.TransformPositionId] ?? new Vector3d());
             var orientation = (Vector3d)(transform[ILayerObject.TransformOrientationId] ?? new Vector3d());
             var angleX = (double)(transform[ILayerObject.TransformXAngleId] ?? 0.0);
             var angleY = (double)(transform[ILayerObject.TransformYAngleId] ?? 0.0);
@@ -394,6 +390,105 @@ namespace NiVE3.PresetPlugin.Renderer
                 .RotateY(-Math.Atan2(x, z) / Math.PI * 180.0)
                 .Translate(pos256.GetElement(0), pos256.GetElement(1), -pos256.GetElement(2))
                 .Scale(1.0, 1.0, -1.0);
+        }
+
+        static Matrix4x4d GetLightMatrix(LightSetting lightSetting, double renderWidth, double renderHeight)
+        {
+            return GetLightMatrix(lightSetting.LightType, lightSetting.Position, lightSetting.PointOfInterest, lightSetting.Orientation, lightSetting.AngleX, lightSetting.AngleY, lightSetting.AngleZ, renderWidth, renderHeight);
+        }
+
+        static Matrix4x4d GetLightMatrix(LightType lightType, PropertyValueGroup transform, double renderWidth, double renderHeight)
+        {
+            return GetLightMatrix(
+                lightType,
+                (Vector3d)(transform[ILayerObject.TransformPositionId] ?? new Vector3d()),
+                (Vector3d)(transform[ILayerObject.TransformPointOfInterestId] ?? new Vector3d()),
+                (Vector3d)(transform[ILayerObject.TransformOrientationId] ?? new Vector3d()),
+                (double)(transform[ILayerObject.TransformXAngleId] ?? 0.0),
+                (double)(transform[ILayerObject.TransformXAngleId] ?? 0.0),
+                (double)(transform[ILayerObject.TransformXAngleId] ?? 0.0),
+                renderWidth,
+                renderHeight
+            );
+        }
+
+        static Matrix4x4d GetLightMatrix(LightType lightType, in Vector3d pos, in Vector3d poi, in Vector3d orientation, double angleX, double angleY, double angleZ, double renderWidth, double renderHeight)
+        {
+            var size = Math.Max(renderWidth, renderHeight);
+            var pos256 = Avx.Divide(pos.AsVector256(), Vector256.Create(size));
+            switch (lightType)
+            {
+                case LightType.Spot:
+                case LightType.Parallel:
+                    {
+                        var poi256 = Avx.Divide(poi.AsVector256(), Vector256.Create(size));
+
+                        var diff = Avx.Subtract(poi256, pos256);
+                        var x = diff.GetElement(0);
+                        var y = diff.GetElement(1);
+                        var z = diff.GetElement(2);
+
+                        var viewMatrix = Matrix4x4d.Identity
+                            .Scale(1.0, 1.0, -1.0)
+                            .Translate(-pos256.GetElement(0), -pos256.GetElement(1), pos256.GetElement(2))
+                            .RotateY(Math.Atan2(x, z) / Math.PI * 180.0)
+                            .RotateX(-Math.Atan2(y, Math.Sqrt(x * x + z * z)) / Math.PI * 180.0);
+
+                        return viewMatrix.RotateX(orientation.X)
+                            .RotateY(orientation.Y)
+                            .RotateZ(orientation.Z)
+                            .RotateX(angleX)
+                            .RotateY(angleY)
+                            .RotateZ(angleZ);
+                    }
+                case LightType.Point:
+                    return Matrix4x4d.CreateTranslate(-pos256.GetElement(0), -pos256.GetElement(1), -pos256.GetElement(2));
+                default:
+                    return Matrix4x4d.Identity;
+            }
+        }
+
+        static Matrix4x4d GetInvertedLightMatrix(LightType lightType, PropertyValueGroup transform, double renderWidth, double renderHeight)
+        {
+            var pos = (Vector3d)(transform[ILayerObject.TransformPositionId] ?? new Vector3d());
+
+            var size = Math.Max(renderWidth, renderHeight);
+            var pos256 = Avx.Divide(pos.AsVector256(), Vector256.Create(size));
+            switch (lightType)
+            {
+                case LightType.Spot:
+                case LightType.Parallel:
+                    {
+                        var poi = (Vector3d)(transform[ILayerObject.TransformPointOfInterestId] ?? new Vector3d());
+                        var orientation = (Vector3d)(transform[ILayerObject.TransformOrientationId] ?? new Vector3d());
+                        var angleX = (double)(transform[ILayerObject.TransformXAngleId] ?? 0.0);
+                        var angleY = (double)(transform[ILayerObject.TransformYAngleId] ?? 0.0);
+                        var angleZ = (double)(transform[ILayerObject.TransformZAngleId] ?? 0.0);
+
+                        var poi256 = Avx.Divide(poi.AsVector256(), Vector256.Create(size));
+
+                        var diff = Avx.Subtract(poi256, pos256);
+                        var x = diff.GetElement(0);
+                        var y = diff.GetElement(1);
+                        var z = diff.GetElement(2);
+
+                        return Matrix4x4d.Identity
+                            .RotateZ(-angleZ)
+                            .RotateY(-angleY)
+                            .RotateX(-angleX)
+                            .RotateZ(-orientation.Z)
+                            .RotateY(-orientation.Y)
+                            .RotateX(-orientation.X)
+                            .RotateX(Math.Atan2(y, Math.Sqrt(x * x + z * z)) / Math.PI * 180.0)
+                            .RotateY(-Math.Atan2(x, z) / Math.PI * 180.0)
+                            .Translate(pos256.GetElement(0), pos256.GetElement(1), -pos256.GetElement(2))
+                            .Scale(1.0, 1.0, -1.0);
+                    }
+                case LightType.Point:
+                    return Matrix4x4d.CreateTranslate(pos256.GetElement(0), pos256.GetElement(1), pos256.GetElement(2));
+                default:
+                    return Matrix4x4d.Identity;
+            }
         }
 
         static bool IntersectLine(in Vector2d v1, in Vector2d v2, in Vector2d v3, in Vector2d v4)
