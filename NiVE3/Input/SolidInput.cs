6550f7c535056f3ef95fc275888cae400cbe547a
mes51 <mes.goruba@gmail.com>
2025-01-05 13:22:10 +0900

    SolidInputにIsSupportLoadToGpuを付け忘れていたのを修正

--- NiVE3/Input/SolidInput.cs
+++ NiVE3/Input/SolidInput.cs
@@ -22,7 +22,7 @@ using NiVE3.ViewModel.Input;
 namespace NiVE3.Input
 {
     [Export(typeof(IInput))]
-    [InputMetadata(typeof(SolidInput), "SolidInput", "", "mes51", ID, "", true)]
+    [InputMetadata(typeof(SolidInput), "SolidInput", "", "mes51", ID, "", true, IsSupportLoadToGpu = true)]
     [InternalInput]
     class SolidInput : IInput
     {
